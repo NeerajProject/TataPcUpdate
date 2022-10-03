fb0684fe2af8507483d41a7d16eef61333995bd0
Neeraj Krishnan V M <112699625+NeerajProject@users.noreply.github.com>
2022-10-03 00:40:42 -0400

    Add files via upload

--- tata_contractors_custom/models/res_contractor.py
+++ tata_contractors_custom/models/res_contractor.py
@@ -13,6 +13,18 @@ class TataMeeting(models.Model):
     place_id = fields.Char('res.place',required=True)
     date = fields.Date(string="Date of Meeting")
     contract_ids = fields.Many2many('res.contractors')
+    leads_count = fields.Integer('Leads Count', compute='_compute_leads_count')
+
+    def action_view_opportunity(self):
+        action = self.env['ir.actions.act_window']._for_xml_id('crm.crm_lead_opportunities')
+        action['domain'] = [('tata_meeting_id', '=', self.id)]
+        return action
+
+    def _compute_leads_count(self):
+        lead_data = self.env['crm.lead'].search([('tata_meeting_id', '=', self.id)])
+        for rec in self:
+            rec.leads_count = len(lead_data)
+
 
 class ResContractors(models.Model):
     _name ='res.contractors'
@@ -20,6 +32,29 @@ class ResContractors(models.Model):
     place_id = fields.Many2one('res.place',string='Place')
     mobile = fields.Char(string='Mobile',required=True )
     firm_id = fields.Many2one('res.firm')
+    leads_count = fields.Integer('Leads Count', compute='_compute_leads_count')
+    meet_count = fields.Integer('Leads Count', compute='_compute_meet_count')
+
     _sql_constraints = [
         ('mobile', 'unique(mobile)', 'Mobile Number  already exists!')
     ]
+
+    def action_view_opportunity(self):
+        action = self.env['ir.actions.act_window']._for_xml_id('crm.crm_lead_opportunities')
+        action['domain'] = [('contractor_id', '=', self.id)]
+        return action
+
+    def _compute_leads_count(self):
+        lead_data = self.env['crm.lead'].search([('contractor_id', '=', self.id)])
+        for rec in self:
+            rec.leads_count = len(lead_data)
+
+    def action_view_meet_attended(self):
+        action = self.env['ir.actions.act_window']._for_xml_id('tata_contractors_custom.tata_meeting_act_window')
+        action['domain'] = [('contract_ids', 'in', self.id)]
+        return action
+
+    def _compute_meet_count(self):
+        lead_data = self.env['tata.meeting'].search([('contract_ids', 'in', self.id)])
+        for rec in self:
+            rec.meet_count = len(lead_data)
