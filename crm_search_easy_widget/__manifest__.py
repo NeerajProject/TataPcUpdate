76635db34d44c81132f06b87c763dbdd60c758dd
Neeraj Krishnan V M <112699625+NeerajProject@users.noreply.github.com>
2022-10-27 06:08:29 -0400

    Add files via upload

---
+++ crm_search_easy_widget/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Lead test',
+    'summary': 'Generate Leads/Opportunities based on country, industries, size, etc.',
+    'category': 'Sales/CRM',
+    'version': '1.2',
+    'depends': [
+
+    ],
+    'data': [
+
+    ],
+    'auto_install': True,
+    'assets': {
+        'web.assets_backend': [
+            'crm_search_easy_widget/static/src/js/**/*',
+        ],
+        'web.assets_qweb': [
+            'crm_search_easy_widget/static/src/xml/**/*',
+        ],
+    },
+    'license': 'LGPL-3',
+}
