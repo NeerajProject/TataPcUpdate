98794e0fec3baff9ab44b8b44d32eed11f91a76b
Neeraj Krishnan V M <112699625+NeerajProject@users.noreply.github.com>
2022-10-27 13:54:16 -0400

    search okay but context needed to add

--- crm_search_easy_widget/static/src/js/leads_tree_generate_leads.js
+++ crm_search_easy_widget/static/src/js/leads_tree_generate_leads.js
@@ -12,10 +12,37 @@ odoo.define('crm_search_easy_widget.search_easy_widget', function (require) {
             this._super(...arguments);
         },
 
-        search_based_on_date()
+        search_based_on_date(e)
         {
-        alert("yes my lord namabala")
-        },
+        console.log(e)
+        e.preventDefault();
+        var from_date = $('.tata_from_date_crm_123').val()
+        var to_date = $('.tata_to_date_crm_123').val()
+var action= {
+                type: 'ir.actions.act_window',
+            res_model: 'crm.lead',
+            views: [ [false, "kanban"],  [false, "list"],[false,"form"]],
+            target: '_blank',
+            domain: [["lead_created_date", ">", from_date],["lead_created_date","<",to_date]],
+
+        }
+        console.log(action)
+   return new Promise(resolve => {
+                this.env.bus.trigger('do-action', {
+                    action,
+                    options: {
+                        on_close: () => {
+                            resolve();
+                            this.fetchAndUpdate();
+                        },
+                    },
+                });
+            });
+
+
+
+
+               },
 
         download_databased_xlsx_report()
         {
